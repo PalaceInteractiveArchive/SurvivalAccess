1c146206b5663e311539ca3a50087fe8824b0bd8
Marc DiFilippo <marc@marcdif.com>
2020-12-17 20:42:14 -0500

    Initial commit

---
+++ src/main/java/network/palace/survivalaccess/SurvivalAccess.java
@@ -0,0 +1,14 @@
+package network.palace.survivalaccess;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SurvivalAccess extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
